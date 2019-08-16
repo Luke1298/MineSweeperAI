470f0dbf8d3da098479c64c09241abc412c2a404
Luke Nelson <lukenelson1298@gmail.com>
2019-08-15 21:16:34 -0600

    Added some stuff...

--- game.py
+++ game.py
@@ -34,10 +34,11 @@ class minesweeper:
         allSurroundingDirections = [surroundingSquares[:-2,:-2], surroundingSquares[1:-1,:-2], surroundingSquares[2:, :-2],
                                   surroundingSquares[2:, 1:-1], surroundingSquares[:-2, 1:-1],
                                   surroundingSquares[:-2, 2:], surroundingSquares[1:-1, 2:], surroundingSquares[2:, 2:]]
-
         newBoard = sum(allSurroundingDirections)
         newBoard[np.where(self.board==9)] = 9
         self.board = newBoard.T
+        if 8 in self.board:
+            self.init_board()
         self.startTime = time.time()
 
 
@@ -69,11 +70,11 @@ class minesweeper:
         while self.board[coordinate] != 0:
             self.init_board()
 
-    def clickCell(self, coordinate):
+    def clickCell(self, coordinate, isRecursion=False):
         if (self.isFrist):
             self.firstClick(coordinate)
             self.isFrist = False
-        if (self.record):
+        if (self.record and not isRecursion):
             self.gameRecord[time.time() - self.startTime] = coordinate
         x, y = coordinate
         if self.visible[x][y] != "?":
@@ -81,7 +82,7 @@ class minesweeper:
         elif self.board[coordinate] == 0:
             self.visible[x][y] = "*"
             for cell in self.getSurroundingSquares(coordinate):
-                self.clickCell(cell)
+                self.clickCell(cell, isRecursion=True)
         elif self.board[coordinate] != 9:
             self.visible[x][y] = str(self.board[coordinate])
         else:
@@ -92,7 +93,7 @@ class minesweeper:
         if (self.isFrist):
             self.firstClick(coordinate)
             self.isFrist = False
-        if (self.record):
+        if (self.record and not self.clickCallbackRecursion):
             self.gameRecord[time.time() - self.startTime] = coordinate
         x, y = coordinate
         if self.visible[x][y] != "?":
@@ -102,7 +103,9 @@ class minesweeper:
             button.description = ""
             button.disabled = True
             for cell in self.getSurroundingSquares(coordinate):
+                self.clickCallbackRecursion = True
                 self.gameboard[cell[0]][cell[1]].click()
+                self.clickCallbackRecursion = False
         elif self.board[coordinate] != 9:
             self.visible[x][y] = str(self.board[coordinate])
             button.description = str(self.board[coordinate])
@@ -114,7 +117,7 @@ class minesweeper:
             button.style.button_color = "#F77"
             #clear_output()
             display(self.visible)
-        if sum([sum([c == "?" for c in row]) for row in self.visible]) == self.bombs:
+        if sum([sum([(c == "?" or str(c)=="9.0") for c in row]) for row in self.visible]) == self.bombs:
             self.winGame()
             #clear_output()
             display(self.visible)
@@ -127,6 +130,7 @@ class minesweeper:
                     btn.disabled = True
         self.score = time.time() - self.startTime
         self.visible = "You have won!"
+        self.record = False
 
     def loseGame(self):
         if self.gameboard:
@@ -136,6 +140,7 @@ class minesweeper:
                     btn.disabled = True
         self.score = 10000
         self.visible = "You have lost"
+        self.record = False
 
     def getScore(self):
         self.score = time.time() - self.startTime
@@ -149,7 +154,9 @@ class minesweeper:
 
     def flag(self, coordinate):
         x, y = coordinate
-        self.visible[x][y] = "X"
+        self.visible[x][y] = "9.0"
+        if (self.record):
+            self.gameRecord[time.time() - self.startTime] = ("F", coordinate[0], coordinate[1])
 
     def play(self):
         while self.visible != "You have lost":
@@ -164,8 +171,8 @@ class minesweeper:
 
     def playOnNoteBook(self):
         if (self.visible != "You have lost" and self.visible != "You have won!"):
+            self.clickCallbackRecursion = False
             clear_output(wait=True)
-            self.readyToRefreshGameBoard = False;
             self.gameboard = []
             for r, row in enumerate(self.visible):
                 gameRow = []
@@ -183,6 +190,73 @@ class minesweeper:
         else:
             clear_output()
             display(self.visible)
+
+    def visNextRecord(self, btn):
+        clear_output(wait=True)
+        endGame = False
+        if self.nclick is not None:
+            if len(self.nclick)==2:
+                print(self.nclick)
+                self.saveVisible = self.visible
+                self.clickCell(self.nclick)
+            if len(self.nclick)==3:
+                print(self.nclick)
+                self.visible[self.nclick[1]][self.nclick[2]] = "9.0"
+
+        if type(self.visible) == str:
+            endGame = True
+            self.visible = self.saveVisible
+        self.showBoard = []
+        for r, row in enumerate(self.visible):
+            gameRow = []
+            for c, v in enumerate(row):
+                cord = (r,c)
+                buttonDisctiptions = str(int(float(v))) if (v!="?" and v!="*") else ""
+                buttonColor = "#DDD" if (v!="?") else "#AAA"
+                if endGame and (cord == self.nclick):
+                    buttonColor = "#F77"
+                    buttonDisctiptions = "9.0"
+                elif cord in [i[1] for i in self.gameShow[:self.showIndex+1]]:
+                    buttonColor = "#AEC6CF"
+                    buttonDisctiptions = int(self.board[cord])
+                elif self.visible[cord[0]][cord[1]] == "9.0":
+                    if self.board[cord[0]][cord[1]] == 9:
+                        buttonColor = "#FDFD96"
+                    else:
+                        buttonColor = "#FFB347"
+
+                bToAdd = widgets.HTML(value="""<div style="height: 30px;
+                                                           padding: 0px;
+                                                           width: 30px; text-align: center;
+                                                           background-color:{};">{}</div>""".format(buttonColor, buttonDisctiptions),
+                                      style= {'background-color': buttonColor})
+                bToAdd.style.button_color = buttonColor
+                #bToAdd.on_click(self.clickCellCallback)
+                gameRow.append(bToAdd)
+            self.showBoard.append(gameRow)
+        for row in self.showBoard:
+            display(widgets.HBox(row))
+        if not endGame:
+            self.showIndex+=1
+            self.nclick = self.gameShow[self.showIndex][1]
+            display(self.nextBtn)
+
+    def seeRecordOnNotebook(self):
+        if not self.gameRecord:
+            print("You need to have started a game with record on")
+            raise
+        self.visible = [["?" for i in range(self.width)] for j in range(self.height)]
+        self.gameShow = sorted(self.gameRecord.items())
+        self.showIndex = -1
+        self.nclick = None
+        btnWidth = 128
+        self.nextBtn = widgets.Button(description='Next',
+                       disabled=False,
+                       button_style='info',
+                       layout=Layout(width='{}px'.format(btnWidth),left='{}px'.format(34*self.width - btnWidth)))
+        self.nextBtn.on_click(self.visNextRecord)
+        self.visNextRecord(None)
+
 
     def solvePercent(self, percent):
         while sum([f=="?" for f in sum(self.visible, [])])/(self.width * self.height) > percent:
